ef0f5a4850d0f7066f0f64100f010f0cf8b7f561
Michelle Van <michellevan@Michelles-MBP.lan>
2022-06-06 20:20:08 -0400

    chrome extension timer

---
+++ popup.js
@@ -0,0 +1,31 @@
+const timeElement = document.getElementById('time')
+
+const nameElement = document.getElementById('name')
+const timerElement = document.getElementById('timer')
+
+
+function updateTimeElements(){
+  chrome.storage.local.get(["timer"], (res)=> {
+    const time = res.timer?? 0
+    timerElement.textContent = `Timer is at ${time} seconds`
+  })
+  
+  const currentTime = new Date().toLocaleTimeString()
+  timeElement.textContent = `The time is : ${currentTime}`
+}
+
+updateTimeElements()
+setInterval(updateTimeElements, 1000)
+
+
+// chrome.action.setBadgeText({
+//   text: 'TIME',},
+//   () => {console.log('Finished setting badge text.')}
+// )
+
+
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "???"
+  nameElement.textContent = `Your name is ${res.name}`
+})
+
